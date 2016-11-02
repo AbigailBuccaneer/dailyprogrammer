2c182b4160ad25c62c6f28805688864325236659
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2016-11-02 10:56:28 +0000

    Exclude ignored files from Nix source

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "daily-programmer-${version}";
   version = "2016-11-02";
-  src = ./.;
+  src = fetchgitLocal ./.;
   buildInputs = [ cmake ];
 }
